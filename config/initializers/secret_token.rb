804dfa2868f0d1ab1924a9ec87a547a4e8a61835
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2013-12-07 09:36:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParkUMBCApi::Application.config.secret_key_base = '875b3dee63b42e9d5ea911961a4f20d12674092c293bfbf48eabd943614cd0f7f0bd90910dcae9d7d364eb6bb64d9682b80e7201d0aad29c970a7166b9cb2a9e'
